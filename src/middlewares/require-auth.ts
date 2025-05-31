082931a614f04f2847c702c16a2055133edb780c
Ridowan Sikder <ridowan.cse@gmail.com>
2025-05-31 19:35:54 +0600

    Updated modules

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from "express"
 import { NotAuthorizeError } from "../errors/not-authorize-error"
+import { NotAuthenticatedError } from "../errors/not-authenticated-error"
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
-	if (!req.currentUser) throw new NotAuthorizeError()
-	next()
+  if (!req.currentUser) throw new NotAuthenticatedError()
+  next()
 }
--- src/middlewares/set-current-user.ts
+++ src/middlewares/set-current-user.ts
@@ -1,31 +1,24 @@
-import { Request, Response, NextFunction } from 'express';
-import JWT from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express"
+import JWT from "jsonwebtoken"
 interface decodedUser {
-  email: string;
-  id: string;
+  email: string
+  id: string
 }
 declare global {
   namespace Express {
     interface Request {
-      currentUser: decodedUser;
+      currentUser: decodedUser
     }
   }
 }
-export const setCurrentUser = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const setCurrentUser = (req: Request, res: Response, next: NextFunction) => {
   if (!req.session?.JWT) {
-    return next();
+    return next()
   }
   try {
-    const currentUser = JWT.verify(
-      req.session.JWT,
-      process.env.JWT_KEY!
-    ) as decodedUser;
-    req.currentUser = currentUser;
+    const currentUser = JWT.verify(req.session.JWT, process.env.JWT_KEY!) as decodedUser
+    req.currentUser = currentUser
   } catch (err) {}
 
-  next();
-};
+  next()
+}
